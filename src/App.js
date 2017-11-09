060f00386d880204c4a1bc50b75297d9c38c3e73
trump.wang <trump.wang@ringcentral.com>
2017-11-09 17:24:58 +0800

    1. add eslint

--- src/App.js
+++ src/App.js
@@ -4,62 +4,57 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  View
-} from 'react-native';
-import { Container, Button, Text } from 'native-base';
+import React from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import {Button, Text} from 'native-base';
 
 const instructions = Platform.select({
-  ios: 'You are using ios',
-  android: 'You are using android',
+    ios: 'You are using ios',
+    android: 'You are using android',
 });
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-
-        <View>
-          <Button style={styles.button}>
-            <Text>
-              Button 2
-            </Text>
-          </Button>
-        </View>
-      </View>
-    );
-  }
-}
-
-function onPressLearnMore() {}
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 60,
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  button: {
-    justifyContent: 'center'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        marginTop: 60,
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        justifyContent: 'center'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+
+                <View>
+                    <Button style={styles.button}>
+                        <Text>
+                            Button 2
+                        </Text>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+};
+
