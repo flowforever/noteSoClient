c4a66674b8df7fbab0216d4ee9d6b2b65b1e49b5
trump.wang <trump.wang@ringcentral.com>
2017-12-14 16:19:46 +0800

    1. add progress bar

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,34 +1,70 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 import * as Progress from 'react-native-progress';
-
+import {NavigationActions} from 'react-navigation';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
         backgroundColor: '#F5FCFF',
     },
-    button: {
-        justifyContent: 'center',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
+    loadingText: {
         marginBottom: 5,
     },
+    progress: {
+        marginBottom: 15,
+    }
 });
 
-export default function Splash() {
-    return <View style={styles.container}>
-        <Text style={styles.welcome}> Loading </Text>
-        <Progress.Bar progress={0.3} width={200} />
-    </View>;
+const timeFrame = 200;
+
+
+export default class Splash extends React.Component {
+
+    constructor(props, ctx) {
+        super(props, ctx);
+
+        this.state = {
+            progress: 0,
+        };
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            if (this.state.progress === 1) {
+                clearInterval(this.timer);
+                setTimeout(() => {
+                    let toHome = NavigationActions.reset({
+                        index: 0,
+                        actions: [NavigationActions.navigate({routeName: 'Home'})],
+                    });
+                    this.props.navigation.dispatch(toHome);
+                }, timeFrame);
+            } else {
+                let random = Math.random() * 0.5;
+                let progress = this.state.progress + random;
+                if (progress > 1) {
+                    progress = 1;
+                }
+                this.setState({progress});
+            }
+        }, timeFrame);
+
+    }
+
+    render() {
+        const width = Dimensions.get('window').width * 0.64;
+
+        return <View style={styles.container}>
+            <Text style={styles.loadingText}>Loading...</Text>
+            <View style={styles.progress}>
+                <Progress.Bar progress={this.state.progress} width={width} height={2}/>
+            </View>
+        </View>;
+    }
 }
 
 Splash.navigationOptions = {
